d354a0ff881e1337cd81bf167ccece928621aaac
Juan Torchia <j.s.torchia@gmail.com>
2021-01-14 18:16:19 -0300

    Comenzamos a utilizar los hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCard'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </div>
 )
