077da4212a13fd81d4181611c567c3ca886fbdb2
Juan Torchia <j.s.torchia@gmail.com>
2021-01-15 23:28:16 -0300

    Configurando Hooks

--- src/components/Logo/style.js
+++ src/components/Logo/style.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const Svg = styled.svg`
-  width: 220px;
-  margin-left: -10px;
-  margin-top: -30px;
+    margin-left: 20%;
+    margin-top: -5px;
+    margin-right: auto;
+    display: block;
 `
